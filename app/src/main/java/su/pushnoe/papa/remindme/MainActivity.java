70e9232c8862d06a5a402cc4d10cb264ec898f2f
papa <mbr0969@gmail.com>
2017-06-12 00:30:17 +0300

    Create MainActivity class and layout

---
+++ app/src/main/java/su/pushnoe/papa/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package su.pushnoe.papa.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by papa on 12.06.17.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
