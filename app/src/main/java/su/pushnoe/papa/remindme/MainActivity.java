98a4b1de8aee2370e2c2113b3a77a2722484176d
papa <mbr0969@gmail.com>
2017-06-13 14:40:27 +0300

    Create ToollBar and icon

--- app/src/main/java/su/pushnoe/papa/remindme/MainActivity.java
+++ app/src/main/java/su/pushnoe/papa/remindme/MainActivity.java
@@ -2,14 +2,31 @@ package su.pushnoe.papa.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-/**
- * Created by papa on 12.06.17.
- */
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+
+    private Toolbar toollbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toollbar = (Toolbar) findViewById(R.id.toollbar);
+        toollbar.setTitle(R.string.app_name);
+        toollbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem){
+                return false;
+            }
+        });
+
+        toollbar.inflateMenu(R.menu.menu);
     }
 }
