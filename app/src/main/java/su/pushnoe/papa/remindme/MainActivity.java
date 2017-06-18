7b0fab3d083598263ba2b4446884a70b49ee6b63
papa <mbr0969@gmail.com>
2017-06-18 14:33:53 +0300

    Added  Navigation_Header

--- app/src/main/java/su/pushnoe/papa/remindme/MainActivity.java
+++ app/src/main/java/su/pushnoe/papa/remindme/MainActivity.java
@@ -1,33 +1,45 @@
 package su.pushnoe.papa.remindme;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toollbar;
+    private static final int LAYOUT = R.layout.activity_main;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
+
     }
 
     private void initToolbar() {
-        toollbar = (Toolbar) findViewById(R.id.toollbar);
-        toollbar.setTitle(R.string.app_name);
-        toollbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+        toolbar = (Toolbar) findViewById(R.id.toollbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
             @Override
             public boolean onMenuItemClick(MenuItem menuItem){
                 return false;
             }
         });
 
-        toollbar.inflateMenu(R.menu.menu);
+        toolbar.inflateMenu(R.menu.menu);
     }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+    }
+
 }
