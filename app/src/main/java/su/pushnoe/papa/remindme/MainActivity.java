1b01f81d290d98a83b5d1ca286e3befb2774a7c4
papa <mbr0969@gmail.com>
2017-06-18 19:12:57 +0300

    Реализация navigation view и tab navigation с тестовым фрагментом.

--- app/src/main/java/su/pushnoe/papa/remindme/MainActivity.java
+++ app/src/main/java/su/pushnoe/papa/remindme/MainActivity.java
@@ -1,16 +1,25 @@
 package su.pushnoe.papa.remindme;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import su.pushnoe.papa.remindme.adapter.TabsPagerFragmentAdapter;
+import su.pushnoe.papa.remindme.fragment.Constants;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
 
 
     @Override
@@ -21,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         initToolbar();
         initNavigationView();
-
+        initTabs();
     }
 
     private void initToolbar() {
@@ -40,6 +49,40 @@ public class MainActivity extends AppCompatActivity {
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.view_navigation_open, R.string.view_navigation_close);
+
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()){
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+              return true;
+            }
+        });
+
+    }
+
+    private void initTabs() {
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout;  tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void showNotificationTab(){
+          viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 
 }
